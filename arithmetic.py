e05ee6b4f5972bbfad4173a38949030f6b1c1318
Adele Teoh <vagrant@vagrant.vm>
2021-01-06 10:56:36 +0000

    Add subtract, multiply, divide, square, cube functions

--- arithmetic.py
+++ arithmetic.py
@@ -6,31 +6,43 @@ def add(num1, num2):
 
     total = num1 + num2
     return total
-    # return 10    ---- not correct
+    # return 10    # removed; not correct
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
+    return (num1 - num2)
+
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
+
+    return (num1 * num2)
 
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
+    return (num1 / num2)
+
 
 def square(num1):
     """Return the square of the input."""
+
+    return (num1 * num1)
 
 
 def cube(num1):
     """Return the cube of the input."""
 
+    return (num1 * num1 * num1)
+
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+
+    return (num1)
 
 
 def mod(num1, num2):
